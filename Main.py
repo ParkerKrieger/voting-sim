db0b857a68978d07d292369d0b5082699db53c3d
Kelsye Anderson <kelsye.anderson@gmail.com>
2022-04-27 18:32:17 -0600

    Added code for graphing different siulation tests

--- Main.py
+++ Main.py
@@ -1,15 +1,26 @@
 from Ranked import Ranked
 from Plurality import Plurality
 from Veto import Veto
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 if __name__ == '__main__':
-    voters = 100
-    candidates = 10
+    voters = 20
+    candidates = 4
     iterations = 1
-    accMean = 0.1 #0.25
-    accDev = 0.01
+    accMean = 0.5
+    accDev = 0.1
     confMean = 0.5
-    confDev = 0.2
+    confDev = 0.01
+
+    liqPluralityPlt = []
+    pluralityPlt = []
+    liqRankedPlt = []
+    rankedPlt = []
+    liqVetoPlt = []
+    vetoPlt = []
+    y = []
 
     for iteration in range(1,6):
         pluralityLiquidData = [0] * candidates
@@ -34,7 +45,7 @@ if __name__ == '__main__':
 
             plurality = Plurality(voters=voters,
                                   candidates=candidates,
-                                  iterations=1,
+                                  iterations=iterations,
                                   accMean=accMean,
                                   accDev=accDev,
                                   confMean=confMean,
@@ -57,7 +68,7 @@ if __name__ == '__main__':
 
             ranked = Ranked(voters=voters,
                             candidates=candidates,
-                            iterations=1,
+                            iterations=iterations,
                             accMean=accMean,
                             accDev=accDev,
                             confMean=confMean,
@@ -80,7 +91,7 @@ if __name__ == '__main__':
 
             veto = Veto(voters=voters,
                         candidates=candidates,
-                        iterations=1,
+                        iterations=iterations,
                         accMean=accMean,
                         accDev=accDev,
                         confMean=confMean,
@@ -89,11 +100,31 @@ if __name__ == '__main__':
                         )
             winner = veto.runSim(0.1)
             vetoData[winner] += 1
-        print(f"---------Iterations: {iteration}-------------")
-        print(f"Plurality Liquid Data: {pluralityLiquidData}")
-        print(f"Plurality Data: {pluralityData}")
-        print(f"Ranked Liquid Data: {rankedLiquidData}")
-        print(f"Ranked Data: {rankedData}")
-        print(f"Veto Liquid Data: {vetoLiquidData}")
-        print(f"Veto Data: {vetoData}")
+        # print(f"---------Iterations: {iteration}-------------")
+        # print(f"Plurality Liquid Data: {pluralityLiquidData}")
+        # print(f"Plurality Data: {pluralityData}")
+        # print(f"Ranked Liquid Data: {rankedLiquidData}")
+        # print(f"Ranked Data: {rankedData}")
+        # print(f"Veto Liquid Data: {vetoLiquidData}")
+        # print(f"Veto Data: {vetoData}")
+
+        liqPluralityPlt.append(pluralityLiquidData[0])
+        pluralityPlt.append(pluralityData[0])
+        liqRankedPlt.append(rankedLiquidData[0])
+        rankedPlt.append(rankedData[0])
+        liqVetoPlt.append(vetoLiquidData[0])
+        vetoPlt.append(vetoData[0])
+        y.append(iteration)
+
+    plt.plot(y, liqPluralityPlt, label = "Liquid Plurality")
+    plt.plot(y, liqRankedPlt, label="Liquid Ranked")
+    plt.plot(y, liqVetoPlt, label="Liquid Veto")
+    plt.plot(y, pluralityPlt, label="Plurality")
+    plt.plot(y, rankedPlt, label="Ranked")
+    plt.plot(y, vetoPlt, label="Veto")
+    plt.legend()
+    plt.title("Low Confidence Mean")
+    plt.xlabel("Iterations")
+    plt.ylabel("# of correct votes")
+    plt.savefig("Graphs/High ConfDev (iterations).png")
 
